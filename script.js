299bd2efc2d6c017939e1ab58832b4d79fddb5a5
isadoramtts13 <isadoramtts13@gmail.com>
2026-06-11 12:25:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarCuriosidade() {
+
+        document.getElementById("curiosidade").innerHTML =
+            "Curiosidade: a Inteligência Artificial pode ajudar agricultores a identificar doenças em plantas antes mesmo que elas sejam visíveis a olho nu.";
+
+            }
